1d89a23b7e231d6dd1ead84430d06278d567671f
Oleksandr Plias <pls777@ukr.net>
2024-03-14 17:43:03 +0200

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,13 @@
+"use strict";
+window.addEventListener("hashchange", ()=>{
+    if (window.location.hash === "#menu") document.body.classList.add("page__body--with-menu");
+    else document.body.classList.remove("page__body--with-menu");
+});
+const form = document.querySelector("form");
+function submit(event) {
+    event.preventDefault();
+    form.reset();
+}
+form.addEventListener("submit", submit);
+
+//# sourceMappingURL=index.f75de5e1.js.map
